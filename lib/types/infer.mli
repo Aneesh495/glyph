48ed7ad406c767e57e4041bfdb0c1c842453c072
Aneesh495 <179398431+Aneesh495@users.noreply.github.com>
2023-12-27 17:29:42 -0500

    Sync typechecker and pretty-printer sources with the Env.t inference API.

--- lib/types/infer.mli
+++ lib/types/infer.mli
@@ -1,15 +1,11 @@
 (** Algorithm W / Hindley–Milner inference for Glyph. *)
 
-(** Infer types for a whole program, returning the enriched environment
-    (prelude + top-level bindings) or a list of diagnostics. *)
 val infer_program :
   ?env:Env.t -> Ast.program -> (Env.t, Diagnostic.t list) result
 
-(** Infer the type of a single expression. *)
 val infer_expr :
   ?env:Env.t -> Ast.expr -> (Ty.ty, Diagnostic.t list) result
 
-(** Infer a pattern against an expected type, returning bound variables. *)
 val infer_pat :
   ?env:Env.t ->
   expected:Ty.ty ->
